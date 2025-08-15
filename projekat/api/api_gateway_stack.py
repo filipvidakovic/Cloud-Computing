77103376bc0895d4572a9b44bfc63ff00b0d29b2
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-08-15 19:56:48 +0200

    [ADD] endpoint for creating artist

--- projekat/api/api_gateway_stack.py
+++ projekat/api/api_gateway_stack.py
@@ -3,15 +3,24 @@ from constructs import Construct
 from ..config import PROJECT_PREFIX
 
 class ApiGateway(Construct):
-    def __init__(self, scope: Construct, id: str, lambdas):
+    def __init__(self, scope: Construct, id: str, auth_lambdas, artist_lambdas):
         super().__init__(scope, id)
 
+        #users
         api = apigw.RestApi(self, f"{PROJECT_PREFIX}AuthApi", rest_api_name=f"{PROJECT_PREFIX}CognitoAuthApi")
 
         api.root.add_resource("register").add_method(
-            "POST", apigw.LambdaIntegration(lambdas.register_lambda)
+            "POST", apigw.LambdaIntegration(auth_lambdas.register_lambda)
         )
 
         api.root.add_resource("login").add_method(
-            "POST", apigw.LambdaIntegration(lambdas.login_lambda)
+            "POST", apigw.LambdaIntegration(auth_lambdas.login_lambda)
         )
+
+        # artists
+        artists_resource = api.root.add_resource("artists")
+        artists_resource.add_method(
+            "POST",
+            apigw.LambdaIntegration(artist_lambdas.create_artist_lambda)
+        )
+
