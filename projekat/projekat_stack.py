c68e106e5b0d9f80c7d74b7e97dcce20ca1a9be6
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-08-15 19:56:11 +0200

    [ADD] artist table and lambdas to project

--- projekat/projekat_stack.py
+++ projekat/projekat_stack.py
@@ -1,10 +1,12 @@
 from aws_cdk import (
     Duration,
     Stack,
+    aws_dynamodb as dynamodb,
     aws_sqs as sqs,
 )
 from constructs import Construct
 from projekat.api.api_gateway_stack import ApiGateway
+from projekat.artists.artists_lambdas import ArtistLambdas
 from projekat.auth.auth_lambda import AuthLambdas
 from projekat.auth.cognito_stack import CognitoAuth
 from projekat.config import PROJECT_PREFIX
@@ -14,6 +16,18 @@ class ProjekatStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # tables
+        self.artist_table = dynamodb.Table(
+            self, "ArtistTable",
+            partition_key=dynamodb.Attribute(
+                name="artistId",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+
         cognito = CognitoAuth(self, f"{PROJECT_PREFIX}Cognito")
-        lambdas = AuthLambdas(self, f"{PROJECT_PREFIX}Lambdas", user_pool=cognito.user_pool, user_pool_client=cognito.user_pool_client)
-        ApiGateway(self, f"{PROJECT_PREFIX}ApiGateway", lambdas=lambdas)
+        auth_lambdas = AuthLambdas(self, f"{PROJECT_PREFIX}Lambdas", user_pool=cognito.user_pool, user_pool_client=cognito.user_pool_client)
+        artist_lambdas = ArtistLambdas(self, "ArtistLambdas", artist_table=self.artist_table)
+        ApiGateway(self, f"{PROJECT_PREFIX}ApiGateway", auth_lambdas=auth_lambdas, artist_lambdas=artist_lambdas)
+
